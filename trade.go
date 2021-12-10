8297a9318d228538b90340dd8f0503054f69d205
Roman Hoog Antink <rha@open.ch>
2021-12-10 01:04:32 +0100

    accept trade duration as command line argument

--- trade.go
+++ trade.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strconv"
 	"time"
 	client2 "tradingClient/client"
 )
@@ -10,9 +13,23 @@ const login = "test"
 const password = "test"
 
 func main() {
+	tradeDuration := parseArgTradeDuration()
+
 	client := client2.NewClient(hostname, login, password)
 
-	client.Observe(time.Now().Add(30 * time.Second))
+	client.Observe(time.Now().Add(20 * time.Second))
 
-	client.Trade(time.Now().Add(1 * time.Minute))
+	client.Trade(time.Now().Add(tradeDuration))
+}
+
+func parseArgTradeDuration() time.Duration {
+	tradeDuration := 1 * time.Minute
+	if len(os.Args) > 1 {
+		minutes, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalf("failed to parse trading duration (minutes) '%v': %v\n", os.Args[1], err)
+		}
+		tradeDuration = time.Duration(minutes * int(time.Minute))
+	}
+	return tradeDuration
 }
